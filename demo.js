17b602d23dd697b34102fb075048fb5e26e862d4
lvbin <472245767@qq.com>
2018-09-28 22:16:57 +0800

    third commit

--- demo.js
+++ demo.js
@@ -7,8 +7,8 @@ var fs = require('fs');
  fs.mkdirSync('css'); // mkdir css
  fs.mkdirSync('js'); // mkdir js
 
- fs.writeFileSync("./index.html", ""); // echo "" >> index.html
- fs.writeFileSync("css/style.css", ""); // echo "" >> css/style.css
- fs.writeFileSync("./js/main.js", ""); // echo "" >> js/main.js
+ fs.writeFileSync("./index.html", "<!DOCTYPE><title>Hello</title><h1>Hi</h1>"); // echo "" >> index.html
+ fs.writeFileSync("css/style.css", "h1{color: red;}"); // echo "" >> css/style.css
+ fs.writeFileSync("./js/main.js", "var string = 'Hello World';alert(string)"); // echo "" >> js/main.js
 
  process.exit(0); // exit 0
